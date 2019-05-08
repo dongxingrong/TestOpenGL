9b0212e059cbfd3a7db3746aa13c2846571497e3
dongxingrong <dongxingrong@163.com>
2019-05-08 18:29:01 +0800

    光照

---
+++ light/Light.h
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 2019/5/8.
+//
+
+#ifndef UNTITLED_LIGHT_H
+#define UNTITLED_LIGHT_H
+
+#include <glfw3.h>
+
+class Light {
+public:
+    Light();
+    void show_light(GLFWwindow* window);
+};
+#endif //UNTITLED_LIGHT_H
